ebf47b36ac97e95131f3e1458f1f7764a6ac9b08
Khairil <sangbima.net@gmail.com>
2020-11-26 19:33:53 +0700

    replace db to mysql and add table wallet

--- controllers/WalletController.php
+++ controllers/WalletController.php
@@ -8,6 +8,7 @@ use app\models\search\SearchWallet;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * WalletController implements the CRUD actions for Wallet model.
@@ -20,6 +21,17 @@ class WalletController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['index', 'view', 'create', 'update', 'delete'],
+                'rules' => [
+                    [
+                        'actions' => ['index', 'view', 'create', 'update', 'delete'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
